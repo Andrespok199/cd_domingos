c7b2a4db996a7f6646c4a1bf315ff381d6bafa0c
Ramon Rodriguez <ramon.rodriguez.mt.@correo.usa.edu.co>
2021-11-09 23:38:16 -0500

    first commit

---
+++ src/main/java/reto3/ciclo3/Ciclo3Application.java
@@ -0,0 +1,20 @@
+//modelo o identidad
+//interface
+//repositorio
+//servicios
+//controlador O web
+
+
+package reto3.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+}
---
+++ src/main/java/reto3/ciclo3/InterfaceBike.java
@@ -0,0 +1,12 @@
+
+package reto3.ciclo3;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author ramon rodriquez
+ */
+public interface InterfaceBike extends CrudRepository<Bike,Integer>{
+    
+}
